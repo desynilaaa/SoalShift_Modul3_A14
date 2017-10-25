70abbb0ae0b4d67484576af154e7a56a1032b6c9
desynilaaa <32993903+desynilaaa@users.noreply.github.com>
2017-10-25 15:03:10 +0700

    Update soal3.c

--- MODUL3SISOP/soal3.c
+++ MODUL3SISOP/soal3.c
@@ -40,11 +40,44 @@ void* kepiting()
 	}
 }
 
+void* input()
+{
+	int pilihan;
+	while(1)
+	{
+		printf("1. Beri makan lohan.\n");
+		printf("2. Beri makan kepiting\n");
+		printf("Masukan pilihan Anda ");
+
+		system(clear);
+		if(pilihan==1)
+		{
+			stt_lohan +=10;
+			if(stt_lohan > 100 || stt_lohan <=0)
+			{
+				status=1;
+				break;
+			}
+		}
+		if(pilihan==2)
+		{
+			stt_kepiting += 10;
+			if(stt_kepiting > 100 || stt_kepiting <= 0)
+			{
+				status=1;
+				break;
+			}
+
+		}
+	}
+}
+
 int main()
 {
 	system("clear");
 	pthread_create(&(trd_lohan), NULL, &lohan, NULL);
 	pthread_create(&(trd_kepiting), NULL, &kepiting, NULL);
+	pthread_create(&(trd_input), NULL, &input, NULL);
 
 	while(1)
 	{
