70eb148f4049aee03c8c07f7ff21fbc625f1d3e4
desynilaaa <32993903+desynilaaa@users.noreply.github.com>
2017-10-25 13:24:53 +0700

    Update soal5.c

--- MODUL3SISOP/soal5.c
+++ MODUL3SISOP/soal5.c
@@ -1,93 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h> //library thread
-#include <dirent.h>
+#include <pthread.h>
+#include <unistd.h>
 #include <string.h>
 
-void *print_message_function( void *ptr );
-void *print_message_function2( void *ptr );
+pthread_t trd[101];
 
-int num =0, num2=0;
-char word[20], word2[20];
-char string[30];
-
-int main(int argc, char*argv[])
+int main(int argc, char *argv[])
 {
-	scanf("%s %s",word, word2);
-    
-     pthread_t thread1, thread2;//inisialisasi awal
-     const char *message1 = "Thread 1";
-     const char *message2 = "Thread 2";
-     int  iret1, iret2;
+	int a=0;
 
-     iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);//membuat thread pertama
-     if(iret1)//jika eror
-     {
-         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
-         exit(EXIT_FAILURE);
-     }
+	for(a=1; a<argc; a++)
+		{
+			pthread_create(&trd[a], NULL, CARI, (void*)argv[i]);
+			printf("Coba : %s\n ", argv[a]);
+		}
 
-     iret2 = pthread_create( &thread2, NULL, print_message_function2, (void*) message2);//membuat thread kedua
-     if(iret2)//jika gagal
-     {
-         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
-         exit(EXIT_FAILURE);
-     }
-
-     //printf("pthread_create() for thread 1 returns: %d\n",iret1);
-     //printf("pthread_create() for thread 2 returns: %d\n",iret2);
-
-     pthread_join( thread1, NULL);
-     pthread_join( thread2, NULL); 
-
-     exit(EXIT_SUCCESS);
-}
-
-void *print_message_function( void *ptr )
-{
-
-	char *path="/bin";
-	DIR* dir;
-
-	dir=opendir(path);
-	FILE *in_file = fopen("/home/desy/MODUL3SISOP/Novel.txt", "r");
-
-	if (in_file == NULL)
-	{
-    	printf("Error file missing\n");
-    	exit(-1);
-	}
-	while(!feof(in_file))//this loop searches the for the current word
-	{
-	    fscanf(in_file,"%s",string);
-	    if(!strcmp(string,word))//if match found increment num
-	    num++;
-	}
-
-	printf("%s : %d \n",word,num );
-}
-
-
-void *print_message_function2( void *ptr )
-{
-
-	char *path="/bin";
-	DIR* dir;
-
-	dir=opendir(path);
-	FILE *in_file = fopen("/home/desy/MODUL3SISOP/Novel.txt", "r");
-
-	if (in_file == NULL)
-	{
-    	printf("Error file missing\n");
-    	exit(-1);
-	}
-	while(!feof(in_file))//this loop searches the for the current word
-	{
-	    fscanf(in_file,"%s",string);
-	    if(!strcmp(string,word2))//if match found increment num
-	    num2++;
-	}
-
-	printf("%s : %d \n",word2,num2 );
+	for(a-0; a<argc; a++)
+		{
+			pthread_join(trd[a], NULL);
+		}
+	return 0;
 }
